f3799b6552dbe53df5489ad55ca86455cad47fd2
Jordan Rivera <101216666+Chiefer230@users.noreply.github.com>
2023-02-14 18:54:17 -0600

    Add files via upload

--- AES.java
+++ AES.java
@@ -1,14 +1,15 @@
 import javax.crypto.*;
 import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class AES {
-     private SecretKey Key;
-     private Cipher Encryption;
-     private int Tlen = 128;
+    private SecretKey Key;
+    private Cipher Encryption;
+    private int Tlen = 128;
 
 
     public void CreateKey() throws NoSuchAlgorithmException {
@@ -22,10 +23,31 @@ public class AES {
         Encryption = Cipher.getInstance("AES/GCM/NoPadding");
         Encryption.init(Cipher.ENCRYPT_MODE,Key);
         byte [] encryptedBytes = Encryption.doFinal(ByteMessages);
-         encrptyedMessage = Base64.getEncoder().encodeToString(encryptedBytes);
+        encrptyedMessage = Base64.getEncoder().encodeToString(encryptedBytes);
         return encrptyedMessage;
     }
-    public String Decrypt(String EncryptedMessage) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+    public String encryptKey(String Message) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        String encrptyedMessage = null;
+        byte[] ByteMessages = Message.getBytes();
+        Encryption = Cipher.getInstance("AES/GCM/NoPadding");
+        Encryption.init(Cipher.ENCRYPT_MODE,Key);
+        byte [] encryptedBytes = Encryption.doFinal(ByteMessages);
+        encrptyedMessage = Base64.getEncoder().encodeToString(encryptedBytes);
+        return encrptyedMessage;
+    }
+    public String Decrypt(String EncryptedMessage,byte [] key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        byte [] decryptedBytes = Base64.getDecoder().decode(EncryptedMessage);
+
+        Cipher decryption = Cipher.getInstance("AES/GCM/NoPadding");
+
+        GCMParameterSpec Spec = new GCMParameterSpec(Tlen,Encryption.getIV());
+        SecretKey S = new SecretKeySpec(key,"AES");
+        decryption.init(Cipher.DECRYPT_MODE,S,Spec);
+        byte [] decrypted = decryption.doFinal(decryptedBytes);
+        return new String(decrypted);
+
+    }
+    public String DecryptKey(String EncryptedMessage) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
         byte [] decryptedBytes = Base64.getDecoder().decode(EncryptedMessage);
         Cipher decryption = Cipher.getInstance("AES/GCM/NoPadding");
         GCMParameterSpec Spec = new GCMParameterSpec(Tlen,Encryption.getIV());
@@ -34,13 +56,16 @@ public class AES {
         return new String(decrypted);
 
     }
+
+    public SecretKey getKey() {
+        return Key;
+    }
+
     public static void main(String [] args) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
         AES N = new AES();
         N.CreateKey();
-            String enc = N.encrypt("Hey");
-            String de = N.Decrypt(enc);
-            System.out.println(enc);
-            System.out.println(de);
+        String enc = N.encrypt("Hey");
+        System.out.println(enc);
 
     }
 }
--- RSA.java
+++ RSA.java
@@ -1,8 +1,7 @@
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
+import javax.crypto.*;
+import java.awt.event.MouseAdapter;
+import java.io.IOException;
+import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.security.*;
 import java.util.Base64;
@@ -37,6 +36,27 @@ public class RSA {
         byte[] decrypted = decryption.doFinal(decryptedBytes);
         return new String(decrypted, "UTF8");
     }
+    public Serializable DecryptKey(SealedObject Key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException, ClassNotFoundException {
+        Cipher decryption = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        decryption.init(Cipher.DECRYPT_MODE, Private);
+        SecretKey unsealKey = (SecretKey) Key.getObject(decryption);
+        return unsealKey;
+    }
+    public SealedObject encryptKey(SecretKey Key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
+        Cipher Encryption = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        Encryption.init(Cipher.ENCRYPT_MODE, Public);
+        SealedObject sealedKey = new SealedObject(Key,Encryption);
+        return sealedKey;
+    }
+
+    public PrivateKey getPrivate() {
+        return Private;
+    }
+
+    public PublicKey getPublic() {
+        return Public;
+    }
+
     public static void main(String [] args) throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, UnsupportedEncodingException {
         RSA rsa = new RSA();
         String enc = rsa.encrypt("Hello");
