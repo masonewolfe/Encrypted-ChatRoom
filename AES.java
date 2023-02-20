a5e10dee10994b39df9b93f08adfb689c1bfbc5a
Jordan Rivera <101216666+Chiefer230@users.noreply.github.com>
2023-02-20 16:41:24 -0600

    Add files via upload

--- AES.java
+++ AES.java
@@ -6,17 +6,34 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
+/** This Class is used to Implement the AES Algorithm into our Hybrid Encryption
+ *
+ */
 public class AES {
-    private SecretKey Key;
-    private Cipher Encryption;
-    private int Tlen = 128;
+    private SecretKey Key;      // AES Key
+    private Cipher Encryption;  // Block Cipher Used
+    private int Tlen = 128;     // Length for the Authentication Tag
 
-
+    /** This Method is used to Generate an AES Key using the 128 bits which transfers to a 10 round encryption and decryption process
+     *
+     * @throws NoSuchAlgorithmException- Encryption Algorithm does not exist
+     */
     public void CreateKey() throws NoSuchAlgorithmException {
         KeyGenerator Generator = KeyGenerator.getInstance("AES");
         Generator.init(128);
         Key = Generator.generateKey();
     }
+
+    /** This Method Encrypts a Message by taking the Bytes of the Message and encrypting it with AES and Mode of Operation GCM (Galois Counter Mode)
+     * The Single AES generated Key is Used to encrypt the Message
+     * @param Message the Message to be encrypted
+     * @return The Message Encoded in Base64
+     * @throws NoSuchPaddingException - Padding does not exist
+     * @throws NoSuchAlgorithmException - Encryption Algorithm does not exist
+     * @throws InvalidKeyException - Incorrect Key length or encoding
+     * @throws IllegalBlockSizeException - Block Cipher size must match
+     * @throws BadPaddingException - Prevents Incorrect Padding
+     */
     public String encrypt(String Message) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         String encrptyedMessage = null;
         byte[] ByteMessages = Message.getBytes();
@@ -26,15 +43,21 @@ public class AES {
         encrptyedMessage = Base64.getEncoder().encodeToString(encryptedBytes);
         return encrptyedMessage;
     }
-    public String encryptKey(String Message) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        String encrptyedMessage = null;
-        byte[] ByteMessages = Message.getBytes();
-        Encryption = Cipher.getInstance("AES/GCM/NoPadding");
-        Encryption.init(Cipher.ENCRYPT_MODE,Key);
-        byte [] encryptedBytes = Encryption.doFinal(ByteMessages);
-        encrptyedMessage = Base64.getEncoder().encodeToString(encryptedBytes);
-        return encrptyedMessage;
-    }
+
+    /**
+     *
+     /** This Method Decrypts an encrypted message using the Private Key and the Block Cipher mode GCM
+     *
+     * @param EncryptedMessage RSA encrypted Message
+     * @param key byte array of the AES Key
+     * @return Decrypted Message or the Original Message before Encryption
+     * @throws NoSuchPaddingException- Padding does not exist
+     * @throws NoSuchAlgorithmException- Encryption Algorithm does not exist
+     * @throws InvalidKeyException- Incorrect Key length or encoding
+     * @throws IllegalBlockSizeException- Block Cipher size must match
+     * @throws BadPaddingException- Prevents Incorrect Padding
+     * @throws InvalidAlgorithmParameterException - Wrong Algorithm
+     */
     public String Decrypt(String EncryptedMessage,byte [] key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
         byte [] decryptedBytes = Base64.getDecoder().decode(EncryptedMessage);
 
@@ -47,16 +70,11 @@ public class AES {
         return new String(decrypted);
 
     }
-    public String DecryptKey(String EncryptedMessage) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
-        byte [] decryptedBytes = Base64.getDecoder().decode(EncryptedMessage);
-        Cipher decryption = Cipher.getInstance("AES/GCM/NoPadding");
-        GCMParameterSpec Spec = new GCMParameterSpec(Tlen,Encryption.getIV());
-        decryption.init(Cipher.DECRYPT_MODE,Key,Spec);
-        byte [] decrypted = decryption.doFinal(decryptedBytes);
-        return new String(decrypted);
 
-    }
-
+    /** Accessor for the AES generated key
+     *
+     * @return
+     */
     public SecretKey getKey() {
         return Key;
     }
--- RSA.java
+++ RSA.java
@@ -1,15 +1,22 @@
 import javax.crypto.*;
-import java.awt.event.MouseAdapter;
 import java.io.IOException;
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.security.*;
 import java.util.Base64;
 
+/** This Class is Used to Implement the RSA Algorithm for our Hybrid Encryption
+ *
+ */
 public class RSA {
+
     private PrivateKey Private;
     private PublicKey Public;
 
+    /** The Constructor is Used to Generate the Key pair for the Client using a key size of 1048 bits
+     *  Both the Private key and the Public Key is generated for client using the RSA Algorithm
+     * @throws NoSuchAlgorithmException - Encryption Algorithm does not exist
+     */
     public RSA() throws NoSuchAlgorithmException {
         KeyPairGenerator Generator = KeyPairGenerator.getInstance("RSA");
         Generator.initialize(1048);
@@ -19,6 +26,16 @@ public class RSA {
 
     }
 
+    /** This Method Encrypts a Message by taking the Bytes of the Message and encrypting it with RSA, Mode of Operation ECB (Electronic Code Book) and the Use of PKCS Padding as the Cipher
+     * The Public Key is Used to encrypt the Message
+     * @param Message the Message to be encrypted
+     * @return The Message Encoded in Base64
+     * @throws NoSuchPaddingException - Padding does not exist
+     * @throws NoSuchAlgorithmException - Encryption Algorithm does not exist
+     * @throws InvalidKeyException - Incorrect Key length or encoding
+     * @throws IllegalBlockSizeException - Block Cipher size must match
+     * @throws BadPaddingException - Prevents Incorrect Padding
+     */
     public String encrypt(String Message) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
         String encrptyedMessage = null;
         byte[] ByteMessages = Message.getBytes();
@@ -29,30 +46,76 @@ public class RSA {
         return encrptyedMessage;
     }
 
-    public String Decrypt(String EncryptedMessage) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, UnsupportedEncodingException {
+    /** This Method Decrypts an encrypted message using the Private Key and the Block Cipher mode ECB and PKCS Padding
+     *
+     * @param EncryptedMessage RSA encrypted Message
+     * @return Decrypted Message or the Original Message before Encryption
+     * @throws NoSuchPaddingException- Padding does not exist
+     * @throws NoSuchAlgorithmException- Encryption Algorithm does not exist
+     * @throws InvalidKeyException- Incorrect Key length or encoding
+     * @throws IllegalBlockSizeException- Block Cipher size must match
+     * @throws BadPaddingException- Prevents Incorrect Padding
+     * @throws InvalidAlgorithmParameterException - Wrong Algorithm
+     * @throws UnsupportedEncodingException - Encoding is not supported
+     */
+    public String Decrypt(String EncryptedMessage) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
         byte[] decryptedBytes = Base64.getDecoder().decode(EncryptedMessage);
         Cipher decryption = Cipher.getInstance("RSA/ECB/PKCS1Padding");
         decryption.init(Cipher.DECRYPT_MODE, Private);
         byte[] decrypted = decryption.doFinal(decryptedBytes);
         return new String(decrypted, "UTF8");
     }
-    public Serializable DecryptKey(SealedObject Key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException, ClassNotFoundException {
+
+    /** This Method is used to Decrypt Key Objects
+     *
+     * @param Object Sealed Object that requires Decryption
+     * @return The Object Unsealed using the RSA Private Key
+     * @throws NoSuchPaddingException- Padding does not exist
+     * @throws NoSuchAlgorithmException- Encryption Algorithm does not exist
+     * @throws InvalidKeyException- Incorrect Key length or encoding
+     * @throws IllegalBlockSizeException- Block Cipher size must match
+     * @throws BadPaddingException- Prevents Incorrect Padding
+     * @throws InvalidAlgorithmParameterException - Wrong Algorithm
+     * @throws IOException - Used for failed or Interrupted I/O operations
+     * @throws ClassNotFoundException - Tries to find a class that exists
+     */
+    public Serializable DecryptObject(SealedObject Object) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, IOException, ClassNotFoundException {
         Cipher decryption = Cipher.getInstance("RSA/ECB/PKCS1Padding");
         decryption.init(Cipher.DECRYPT_MODE, Private);
-        SecretKey unsealKey = (SecretKey) Key.getObject(decryption);
-        return unsealKey;
-    }
-    public SealedObject encryptKey(SecretKey Key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
-        Cipher Encryption = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        Encryption.init(Cipher.ENCRYPT_MODE, Public);
-        SealedObject sealedKey = new SealedObject(Key,Encryption);
-        return sealedKey;
+        Serializable UnsealObject = (Serializable)  Object.getObject(decryption);
+        return UnsealObject;
     }
 
+    /** This Method encrypts a Serizilaible Object Using the RSA Public Key
+     *
+     * @param Object Any Serializer Object that can be encrypted
+     * @return The sealed Object or the encrypted Object using the RSA Public Key
+     * @throws NoSuchPaddingException- Padding does not exist
+     * @throws NoSuchAlgorithmException- Encryption Algorithm does not exist
+     * @throws InvalidKeyException- Incorrect Key length or encoding
+     * @throws IllegalBlockSizeException- Block Cipher size must match
+     * @throws BadPaddingException- Prevents Incorrect Padding
+     * @throws IOException - Used for failed or Interrupted I/O operations
+     */
+    public SealedObject encryptObject(Serializable Object) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
+        Cipher Encryption = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        Encryption.init(Cipher.ENCRYPT_MODE, Public);
+        SealedObject sealedObject = new SealedObject(Object,Encryption);
+        return sealedObject;
+    }
+
+    /** Accessor Method for the Private Key
+     *
+     * @return the Private Key
+     */
     public PrivateKey getPrivate() {
         return Private;
     }
 
+    /** Accessor Method for the Public Key
+     *
+     * @return the Public Key
+     */
     public PublicKey getPublic() {
         return Public;
     }
