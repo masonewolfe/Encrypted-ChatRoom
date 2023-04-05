e50574a3c560525be9a35418b1b0bdcf1307e169
joeysaline <jsaline@albany.edu>
2023-04-04 21:14:20 -0400

    start of authentication

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './routes/Landing.js';
 import LogIn from './routes/LogIn.js';
-import SignUp from './routes/SignUp.js';
+import Register from './routes/Register.js';
 import Messages from './routes/Messages.js';
 import Friends from './routes/Friends.js';
 
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Landing />} />
         <Route path='/login' element={<LogIn />} />
-        <Route path='/signup' element={<SignUp />} />
+        <Route path='/register' element={<Register />} />
         <Route path='/messages' element={<Messages />} />
         <Route path='/friends' element={<Friends/>} />
       </Routes>
--- web-app/src/routes/Register.js
+++ web-app/src/routes/Register.js
@@ -2,30 +2,34 @@ import React from 'react'
 import NavBar from '../components/NavBar.js';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
-export default function LogIn() {
+export default function Register() {
   return (
     <>
-      <NavBar brand="Log In" />
+      <NavBar brand="Register" />
       <div className='bg-dark'>
         <Container className='py-5 text-light text-center'>
           <Form className="">
-            <Form.Group className="my-3 py-5" controlId="formBasicEmail">
+            <Form.Group className="my-3 py-3" controlId="formBasicEmail">
               <Form.Label><h2>Cipher Id</h2></Form.Label>
               <Form.Control type="email" placeholder="Enter cipher id" />
               <Form.Text className="text-muted">
                 Cipher Id's are written in the format xxxxx@cipher.com
               </Form.Text>
             </Form.Group>
-            <Form.Group className="my-3 py-5" controlId="formBasicPassword">
+            <Form.Group className="my-3 py-3" controlId="formBasicPassword">
               <Form.Label><h2>Password</h2></Form.Label>
               <Form.Control type="password" placeholder="Enter password" />
             </Form.Group>
-            <Button className='btn btn-lg mb-5' variant="outline-light" type="submit">
-              Log In
+            <Form.Group className="my-3 py-3" controlId="formBasicPassword">
+              <Form.Label><h2>Confirm password</h2></Form.Label>
+              <Form.Control type="password" placeholder="Re-enter password" />
+            </Form.Group>
+            <Button className='btn btn-lg mb-3' variant="outline-light" type="submit">
+              Create account
             </Button>
           </Form>
-          <div className='pt-5 pb-3'>Don't have an account with cipher?</div>
-          <a href='/register'><Button variant='outline-light'>Register here</Button></a>
+          <div className='pt-5 pb-3'>Already have an account with cipher?</div>
+          <a href='/login'><Button variant='outline-light'>Log in here</Button></a>
           <div className='py-5'>Break free from the grips of big tech companies, and protect what's important to you once and for all.</div>
         </Container>
       </div>
